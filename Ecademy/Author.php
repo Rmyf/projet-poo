0d0078f4245bebfc36195d30a9ebb38957143abd
remyf <remy.furnari@gmail.com>
2017-06-23 14:04:56 +0200

    add point

---
+++ Ecademy/Author.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Comments
+ *
+ * @author tal
+ */
+class Author {
+    
+    public $nom;
+    public $avatar;
+    public $age;
+    public $level;
+    //put your code here
+}
---
+++ Ecademy/Posts.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Posts
+ *
+ * @author tal
+ */
+class Posts {
+    
+    public $categorie;
+    public $titre;
+    public $contenu;
+    public $votes;
+    public $date;
+    public $auteur;
+    
+    public function __construct($categorie, $titre, $contenu, $votes, $date, $auteur) {
+        $this->categorie = $categorie;
+        $this->titre = $titre;
+        $this->contenu = $contenu;
+        $this->votes = $votes;
+        $this->date = $date;
+        $this->auteur = $auteur;
+    }
+
+    //put your code here
+}
